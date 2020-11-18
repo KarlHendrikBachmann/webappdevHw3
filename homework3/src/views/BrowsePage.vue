5b4fa8a70f58b3e35c9b6befe26065121f7ed1a9
Allan <allan.alikas@gmail.com>
2020-11-18 22:05:28 +0200

    Login page

--- homework3/src/views/BrowsePage.vue
+++ homework3/src/views/BrowsePage.vue
@@ -1,5 +1,32 @@
 <template>
-    <h1>BROWSEPAGE</h1>
+   <body>
+    <header>
+      <nav>
+        <div class="logo-container">
+          <img src="../assets/logo.png" alt="postIt">
+        </div>
+        <div class="search-container">
+          <input type="text" name="search"><button type="button">Search</button>
+        </div>
+        <div class="avatar-container">
+          <img class="avatar">
+          <div class="drop-down-container">
+            <span id="user-name">John Doe</span>
+            <span id="user-email"></span>
+            <span class="separator"></span>
+            <span>
+              <a href="browse.html">Browse</a>
+            </span>
+            <span class="separator"></span>
+            <span>
+              <a href="login.html">Log Out</a>
+            </span>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <section class="main-container"></section>
+  </body>
 </template>
 
 <script>
@@ -7,3 +34,7 @@ export default {
     
 }
 </script>
+
+<style>
+@import url('../assets/css/style.css');
+</style>
--- homework3/src/views/LoginPage.vue
+++ homework3/src/views/LoginPage.vue
@@ -1,9 +1,63 @@
 <template>
-    <h1>LOGINPAGE</h1>
+    <body id="app">
+    <section class="login-page">
+      <section id="login-container">
+        <div>
+          <h1>Welcome to postIt!</h1>
+        </div>
+        <div>
+          <h4>
+            <a href="#">Create an Account</a>
+          </h4>
+        </div>
+        <div>
+          <h4>Or</h4>
+        </div>
+        <div>
+          <h3>Please Log In</h3>
+          <form action="#/postitapp" method="post" v-on:submit.capture=onSubmit>
+            <div>
+              <input type="text" name="email" placeholder="Email">
+            </div>
+            <div>
+              <input type="password" name="password" placeholder="Password">
+            </div>
+            <div>
+              <button class="button" @click.prevent="submit_data" name="login">Log In</button>
+            </div>
+            <div>
+              <small>
+                <a href="#">Forgot password?</a>
+              </small>
+            </div>
+          </form>
+        </div>
+      </section>
+    </section>
+  </body>
 </template>
 
 <script>
 export default {
-    
+    name: "app",
+        data() {
+            return {
+                username: '',
+                password: '',
+                response: ''
+            }
+        },
+        methods: {
+            submit_data() {
+                fetch('https://jsonplaceholder.typicode.com/todos/1')
+                    .then(response => response.json())
+                    .then(json => console.log(json))
+                this.$router.push({ path : '/' });
+            }
+    }
 }
 </script>
+
+<style>
+@import url('../assets/css/style.css');
+</style>
