68dea4601048c19696f6f8af009b8f4f0e638c17
Allan <allan.alikas@gmail.com>
2020-11-18 21:31:35 +0200

    3 Page layout

--- homework3/src/router/router.js
+++ homework3/src/router/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '../components/HelloWorld.vue'
+import PostitApp from '../views/PostItApp.vue'
 import LoginPage from '../views/LoginPage.vue'
 import BrowsePage from '../views/BrowsePage.vue'
 
@@ -10,8 +10,8 @@ export default new Router({
     routes: [
         {
             path: '/',
-            name: 'Hello',
-            component: HelloWorld
+            name: 'App',
+            component: PostitApp
         },
         {
             path: '/loginpage',
