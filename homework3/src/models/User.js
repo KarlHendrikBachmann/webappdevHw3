8268f29f5de33e953133ebf10fe4fad01cd1f29b
Karl Hendrik Bachmann <karl.hendrik.bachmann@ut.ee>
2020-11-19 17:38:10 +0200

    Tried using axios, but didn't find a solution, therefore hardcoded the users. Browse page completed. In style.css .main-container changed min-height % to vh.

---
+++ homework3/src/models/User.js
@@ -0,0 +1,8 @@
+export default class User {
+    constructor(firstname, lastname, avatar) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.avatar = avatar;
+        this.selected = false;
+    }
+}
