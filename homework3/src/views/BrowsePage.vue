623d0dc5c87b57c34192b7bda540198613ab4032
Karl Hendrik Bachmann <karl.hendrik.bachmann@ut.ee>
2020-11-22 13:40:32 +0200

    Changed main-container min-height % back to vh again Deleted hardcoded values.

--- homework3/src/views/BrowsePage.vue
+++ homework3/src/views/BrowsePage.vue
@@ -18,23 +18,11 @@
 
 <script>
 
-import User from '../models/User'
 import NavBar from "@/views/NavBar";
 
 export default {
   name: "BrowsePage",
   components: {NavBar},
-  data() {
-    return{
-        userList: [
-          new User("John","Doe","https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"),
-          new User("Gordon","Freeman","https://images.unsplash.com/photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"),
-          new User("John","Doe","https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80"),
-          new User("Bruce","Wayne","https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"),
-          new User("Richard","Stallman","https://images.unsplash.com/photo-1553798194-cc0213ae7f99?ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80")
-        ]
-    }
-  },
   computed: {
     users() {
       return this.$store.state.users
